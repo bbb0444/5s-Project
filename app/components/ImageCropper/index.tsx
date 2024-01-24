0c5cb17326658dca4207ef2cea10183671259671
bryden berrysmith <berrysmithbryden@gmail.com>
2024-01-24 22:25:15 +1300

    working cropper with reset and redo, also hid capture button after capture

---
+++ app/components/ImageCropper/index.tsx
@@ -0,0 +1,166 @@
+import { FC, useEffect, useRef, useState } from "react";
+import Cropper, { Area } from "react-easy-crop";
+import { loadImage } from "../../lib/ImageLoader";
+
+interface Props {
+  source: string;
+  width: number;
+  height: number;
+  zoom: number;
+  rotation: number;
+  position: { x: number; y: number };
+  onZoomChange(zoomValue: number): void;
+  onRotationChange(rotationValue: number): void;
+  onCrop(image: Blob): void;
+  onCropChange(position: { x: number; y: number }): void;
+}
+
+function degreesToRadians(degrees: number): number {
+  return (degrees * Math.PI) / 180;
+}
+
+function rotateSize(width: number, height: number, rotation: number) {
+  const rotRad = degreesToRadians(rotation);
+
+  return {
+    boxWidth:
+      Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
+    boxHeight:
+      Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
+  };
+}
+
+const ImageCropper: FC<Props> = ({
+  source,
+  height,
+  width,
+  zoom,
+  rotation,
+  position,
+  onCrop,
+  onZoomChange,
+  onCropChange,
+  onRotationChange,
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const desiredWidth = width;
+  const desiredHeight = height;
+
+  const handleCrop = (_: Area, croppedAreaPixels: Area) => {
+    try {
+      if (!croppedAreaPixels) return;
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      const image = new Image();
+      image.crossOrigin = "anonymous";
+
+      image.onload = () => {
+        // set the size for canvas
+
+        const { boxWidth, boxHeight } = rotateSize(
+          image.width,
+          image.height,
+          rotation
+        );
+
+        const rotRad = degreesToRadians(rotation);
+        canvas.width = boxWidth;
+        canvas.height = boxHeight;
+
+        ctx.translate(boxWidth / 2, boxHeight / 2);
+        ctx.rotate(rotRad);
+        ctx.translate(-image.width / 2, -image.height / 2);
+
+        // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+        ctx.drawImage(image, 0, 0);
+
+        const croppedCanvas = document.createElement("canvas");
+        const croppedCtx = croppedCanvas.getContext("2d");
+
+        if (!croppedCtx) return;
+
+        const { x, y, width, height } = croppedAreaPixels;
+
+        croppedCanvas.width = desiredWidth;
+        croppedCanvas.height = desiredHeight;
+        croppedCtx.drawImage(
+          canvas,
+          x,
+          y,
+          width,
+          height,
+          0,
+          0,
+          desiredWidth,
+          desiredHeight
+        );
+
+        croppedCanvas.toBlob((blob) => {
+          if (blob) onCrop(blob);
+        }, "image/png");
+      };
+
+      image.src = source;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const calculateSize = async () => {
+    setLoading(true);
+    const containerRect = containerRef.current?.getBoundingClientRect();
+    if (!containerRect) return;
+
+    const { width, height, orientation } = await loadImage(
+      source,
+      containerRect.width * (9 / 16),
+      containerRect.height * (9 / 16)
+    );
+
+    const isPortrait = orientation === "portrait";
+
+    setSize({ width, height: isPortrait ? width : height });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    // this calculation will decide the size of our canvas so that we can have the non destructive UI
+    calculateSize();
+  }, [source, containerRef]);
+
+  return (
+    <div
+      className="relative w-full h-full flex flex-col items-center justify-center"
+      ref={containerRef}
+    >
+      {loading ? (
+        <div>loading</div>
+      ) : (
+        <div
+          // className="relative mx-auto"
+          style={{ width: size.width, height: size.height }}
+        >
+          <Cropper
+            image={source}
+            crop={position}
+            zoom={zoom}
+            aspect={1}
+            showGrid={true}
+            rotation={rotation}
+            onZoomChange={onZoomChange}
+            onCropChange={onCropChange}
+            onRotationChange={onRotationChange}
+            onCropComplete={handleCrop}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageCropper;
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,13 +1,36 @@
 "use client";
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import styles from "./upload.module.scss";
 import Image from "next/image";
 
-function Index() {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const photoRef = useRef(null);
+import ImageCropper from "../components/ImageCropper";
+import { get } from "http";
 
+function Index() {
+  const [mobile, setMobile] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const photoRef = useRef<HTMLCanvasElement>(null);
+  const [photoData, setPhotoData] = useState<string | null>(null);
   const [hasPhoto, setHasPhoto] = useState(false);
+  const [altUpload, setAltUpload] = useState(false);
+
+  const [zoom, setZoom] = useState(1);
+  const [croppedImage, setCroppedImage] = useState<Blob>();
+  const [rotation, setRotation] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleOnZoom = useCallback((zoomValue: number) => {
+    setZoom(zoomValue);
+  }, []);
+
+  const handleOnRotation = useCallback((rotationValue: number) => {
+    setRotation(rotationValue);
+  }, []);
+
+  const uploadPhoto = async () => {
+    if (!croppedImage) return;
+    //
+  };
 
   const getCapture = () => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -29,8 +52,45 @@ function Index() {
           console.error(err);
         });
     } else {
+      setMobile(true);
       console.error("Your browser does not support camera capture");
     }
+  };
+
+  const takePhoto = () => {
+    if (videoRef.current!.videoWidth === 0) {
+      setAltUpload(true);
+      return;
+    }
+
+    const width = videoRef.current!.videoWidth;
+    const height = videoRef.current!.videoHeight;
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo!.width = width;
+    photo!.height = height;
+
+    let context = photo!.getContext("2d");
+    context!.drawImage(video!, 0, 0, width, height);
+    let photoData = photo!.toDataURL(); // Save the photo data to a variable
+    console.log(photoData);
+    setHasPhoto(true);
+    setPhotoData(photoData);
+  };
+
+  const reset = () => {
+    setZoom(1); // Reset zoom to initial value
+    setRotation(0); // Reset rotation to initial value
+    console.log(croppedImage);
+    setPosition({ x: 0, y: 0 });
+    setCroppedImage(undefined); // Clear the cropped image
+  };
+  const redo = () => {
+    setHasPhoto(false);
+    setPhotoData(null);
+    getCapture();
   };
 
   useEffect(() => {
@@ -38,30 +98,71 @@ function Index() {
   }, []);
 
   return (
-    <div>
-      <div className={styles.container}>
-        <Image
-          src={"/SVG/upload.svg"}
-          alt="black and white back face of a digital camera"
-          width={1000} // width of the image file
-          height={1000} // height of the image file
-          className={styles.image}
-        />
-        <div className={styles.captureContainer}>
-          <video className={styles.capture} ref={videoRef}></video>
+    <div className={styles.root}>
+      <div className={styles.parent}>
+        <div className={styles.container}>
+          <div className={styles.image}>
+            <Image
+              src={"/SVG/upload.svg"}
+              alt="black and white back face of a digital camera"
+              width={1000} // width of the image file
+              height={1000} // height of the image file
+              // className={styles.image}
+            />
+            <div className={styles.captureContainer}>
+              {hasPhoto && (
+                <div className={styles.imageCropper}>
+                  <ImageCropper
+                    position={position}
+                    zoom={zoom}
+                    onZoomChange={handleOnZoom}
+                    rotation={rotation}
+                    onRotationChange={setRotation}
+                    source={photoData!}
+                    onCrop={setCroppedImage}
+                    onCropChange={setPosition}
+                    width={1000}
+                    height={1000}
+                  />
+                </div>
+              )}
+              {!hasPhoto && (
+                <video className={styles.video} ref={videoRef}></video>
+              )}
+
+              <div className={styles.capture}>
+                <canvas ref={photoRef}></canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          {!hasPhoto && <button onClick={takePhoto}> capture </button>}
+          {!hasPhoto && altUpload && (
+            <input
+              type="file"
+              accept="image/*"
+              capture={true}
+              onChange={(event) => {
+                if (event.target.files) {
+                  const file = event.target.files[0];
+                  setPhotoData(URL.createObjectURL(file));
+                  setHasPhoto(true);
+
+                  // handle the file
+                }
+              }}
+            />
+          )}
+          {hasPhoto && (
+            <>
+              <button onClick={uploadPhoto}> accept </button>
+              <button onClick={reset}> reset </button>
+              <button onClick={redo}> redo </button>
+            </>
+          )}
         </div>
       </div>
-      <input
-        type="file"
-        accept="image/*"
-        capture={true}
-        onChange={(event) => {
-          if (event.target.files) {
-            const file = event.target.files[0];
-            // handle the file
-          }
-        }}
-      />
     </div>
   );
 }
